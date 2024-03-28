e1f9e88ecf34119df69ed95d7bac0b34904977df
Данила <danila.las.vegas@gmail.com>
2024-03-28 23:49:30 +0300

    ads5 test4

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,63 +6,63 @@
 
 std::string infx2pstfx(std::string inf) {
 TStack<char, 100> stack1;
-    std::string pst;
-    for (int i = 0; i < inf.length(); i++) {
-        if (inf[i] >= '0' && inf[i] <= '9') {
-            pst += inf[i];
-            pst += ' ';
-        } else if (inf[i] == '(') {
-            stack1.push('(');
-        } else if (inf[i] == ')') {
-            while (!stack1.isempty() && stack1.get() != '(') {
-                pst += stack1.get();
-                pst += ' ';
-                stack1.pop();
-            }
-            if (!stack1.isempty()) {
-                stack1.pop();
-            }
-        } else {
-            while (!stack1.isempty()&&stack1.get()!='('&&(inf[i] =='+'||stack1.get() =='*'||stack1.get()=='/')) {
-                pst += stack1.get();
-                pst += ' ';
-                stack1.pop();
-            }
-            stack1.push(inf[i]);
-        }
-    }
-    while (!stack1.isempty()) {
-        pst += stack1.get();
-        pst += ' ';
-        stack1.pop();
-    }
-    return pst;
+  std::string pst;
+  for (int i = 0; i < inf.length(); i++) {
+      if (inf[i] >= '0' && inf[i] <= '9') {
+          pst += inf[i];
+          pst += ' ';
+      } else if (inf[i] == '(') {
+          stack1.push('(');
+      } else if (inf[i] == ')') {
+          while (!stack1.isempty() && stack1.get() != '(') {
+              pst += stack1.get();
+              pst += ' ';
+              stack1.pop();
+          }
+          if (!stack1.isempty()) {
+              stack1.pop();
+          }
+      } else {
+          while (!stack1.isempty()&&stack1.get() != '('&&(inf[i] =='+'||stack1.get() =='*'||stack1.get()=='/')) {
+              pst += stack1.get();
+              pst += ' ';
+              stack1.pop();
+          }
+          stack1.push(inf[i]);
+      }
+  }
+  while (!stack1.isempty()) {
+      pst += stack1.get();
+      pst += ' ';
+      stack1.pop();
+  }
+  return pst;
 }
 
 int eval(std::string post) {
-    TStack<int, 100> stack2;
-    for (int i = 0; i < post.length(); i++) {
-        char ch = post[i];
-        if (isdigit(ch)) {
-            stack2.push(ch - '0');
-        } else if (ch == '+' || ch == '-' || ch == '/' || ch == '*') {
-            int operand2 = stack2.pop();
-            int operand1 = stack2.pop();
-            switch (ch) {
-                case '+':
-                    stack2.push(operand1 + operand2);
-                    break;
-                case '-':
-                    stack2.push(operand1 - operand2);
-                    break;
-                case '*':
-                    stack2.push(operand1 * operand2);
-                    break;
-                case '/':
-                    stack2.push(operand1 / operand2);
-                    break;
-            }
-        }
-    }
-    return stack2.pop();
+  TStack<int, 100> stack2;
+  for (int i = 0; i < post.length(); i++) {
+      char ch = post[i];
+      if (isdigit(ch)) {
+          stack2.push(ch - '0');
+      } else if (ch == '+' || ch == '-' || ch == '/' || ch == '*') {
+          int operand2 = stack2.pop();
+          int operand1 = stack2.pop();
+          switch (ch) {
+              case '+':
+                  stack2.push(operand1 + operand2);
+                  break;
+              case '-':
+                  stack2.push(operand1 - operand2);
+                  break;
+              case '*':
+                  stack2.push(operand1 * operand2);
+                  break;
+              case '/':
+                  stack2.push(operand1 / operand2);
+                  break;
+          }
+      }
+  }
+  return stack2.pop();
 }
