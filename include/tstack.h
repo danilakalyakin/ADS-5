e9ad34d11a60fbf714ad1f7f5201a209672611c5
Данила <danila.las.vegas@gmail.com>
2024-03-28 23:56:28 +0300

    test6

--- include/tstack.h
+++ include/tstack.h
@@ -4,11 +4,11 @@
 
 template<typename T, int size>
 class TStack {
-    private:
+  private :
     T* stackArray;
     int topIndex;
 
-    public:
+  public :
     TStack() {
         stackArray = new T[size];
         topIndex = -1;
